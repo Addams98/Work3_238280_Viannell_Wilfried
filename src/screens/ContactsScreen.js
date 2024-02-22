1bd2e82befae87e457cb21c0a698c67c11f43fec
Addams98 <125551970+Addams98@users.noreply.github.com>
2024-02-22 13:21:39 +0200

    Update ContactsScreen.js

--- src/screens/ContactsScreen.js
+++ src/screens/ContactsScreen.js
@@ -3,8 +3,8 @@ import { View, Text, StyleSheet, FlatList, TextInput, TouchableOpacity } from 'r
 
 const ContactsScreen = () => {
   const [contactsData, setContactsData] = useState([
-    { id: '1', name: 'Ndoba Hakim', phoneNumber: '123-456-7890' },
-    { id: '2', name: 'Keza Ketsia', phoneNumber: '987-654-3210' },
+    { id: '1', name: 'Addams', phoneNumber: '123-456-7890' },
+    { id: '2', name: 'John Wick', phoneNumber: '987-654-3210' },
     { id: '3', name: 'Rwego Jerome', phoneNumber: '456-789-0123' },
     // Add more contacts as needed
   ]);
